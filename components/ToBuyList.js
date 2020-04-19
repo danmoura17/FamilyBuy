299c26f7d1132964c9805b22421ef08ee4056dc4
danmoura17 <danmoura17@gmail.com>
2020-04-19 17:43:05 -0300

    Passing properties to the to buy modal

--- components/ToBuyList.js
+++ components/ToBuyList.js
@@ -1,53 +1,78 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import Colors from '../Colors';
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Modal } from "react-native";
+import Colors from "../Colors";
+import TobuyModal from "./TobuyModal";
 
-export default ToBuyList = ({list}) => {
-    const completedCount = list.items.filter(item => item.completed).length;
-    const remainingCount = list.items. length - completedCount;
+export default class ToBuyList extends React.Component {
+  state = {
+    showListVariable: false,
+  };
+
+  toggleListModel() {
+    this.setState({ showListVariable: !this.state.showListVariable });
+  }
+
+  render() {
+    const list = this.props.list;
+    const completedCount = list.items.filter((item) => item.completed).length;
+    const remainingCount = list.items.length - completedCount;
 
     return (
-        <View style={[styles.listContainer, {backgroundColor: list.color}]}>
-            <Text style={styles.listTitle} numberOfLines={1}>
-                {list.name}
-            </Text>
-            <View>
-                <View style={{alignItems: "center"}}>
-                    <Text style={styles.count}>{completedCount}</Text>
-                    <Text style={styles.subtitle}>Não feito</Text>
-                </View>
-                <View style={{alignItems: "center"}}>
-                    <Text style={styles.count}>{remainingCount}</Text>
-                    <Text style={styles.subtitle}>Feito</Text>
-                </View>
+      <View>
+        <Modal
+          animationType="slide"
+          visible={this.state.showListVariable}
+          onRequestClose={() => this.toggleListModel()}
+        >
+          <TobuyModal list={list} closeModal={() => this.toggleListModel()}/>
+
+        </Modal>
+        <TouchableOpacity
+          style={[styles.listContainer, { backgroundColor: list.color }]}
+          onPress={() => this.toggleListModel()}
+        >
+          <Text style={styles.listTitle} numberOfLines={1}>
+            {list.name}
+          </Text>
+          <View>
+            <View style={{ alignItems: "center" }}>
+              <Text style={styles.count}>{completedCount}</Text>
+              <Text style={styles.subtitle}>Não feito</Text>
             </View>
-        </View>
-    )
+            <View style={{ alignItems: "center" }}>
+              <Text style={styles.count}>{remainingCount}</Text>
+              <Text style={styles.subtitle}>Feito</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    listContainer: {
-        paddingVertical: 32,
-        paddingHorizontal: 16,
-        borderRadius: 6,
-        marginHorizontal: 12,
-        alignItems: "center",
-        width: 200
-    },
-    listTitle:{
-        fontSize: 24,
-        fontWeight: "700",
-        color: Colors.white,
-        marginBottom: 18
-    },
-    count:{
-        fontSize: 48,
-        fontWeight: "200",
-        color: colors.white
-    }, 
-    subtitle: {
-        fontSize: 12,
-        fontWeight: "700",
-        color: colors.white
-    }
-})
+  listContainer: {
+    paddingVertical: 32,
+    paddingHorizontal: 16,
+    borderRadius: 6,
+    marginHorizontal: 12,
+    alignItems: "center",
+    width: 200,
+  },
+  listTitle: {
+    fontSize: 24,
+    fontWeight: "700",
+    color: Colors.white,
+    marginBottom: 18,
+  },
+  count: {
+    fontSize: 48,
+    fontWeight: "200",
+    color: colors.white,
+  },
+  subtitle: {
+    fontSize: 12,
+    fontWeight: "700",
+    color: colors.white,
+  },
+});
