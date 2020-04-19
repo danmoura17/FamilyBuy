da28198bf489c35081a37aee8e6e9c13aa609b90
danmoura17 <danmoura17@gmail.com>
2020-04-19 02:27:25 -0300

    Creating new view with tasks list

---
+++ App.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import colors from "./Colors";
+import tempData from "./tempData";
+import ToBuyList from './components/ToBuyList';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ flexDirection: "row" }}>
+          <View style={styles.divider} />
+          <Text style={styles.title}>
+            Família Moura
+            <Text style={{ fontWeight: "300", color: colors.blue }}> App </Text>
+          </Text>
+        </View>
+        <View style={{ marginVertical: 48 }}>
+          <TouchableOpacity style={styles.addList}>
+            <AntDesign name="plus" size={16} color={colors.blue} />
+          </TouchableOpacity>
+
+          <Text style={styles.add}> Adicionar lista</Text>
+        </View>
+
+        <View style={{ height: 275, paddingLeft: 32 }}>
+          <FlatList
+            data={tempData}
+            keyExtractor={(item) => item.name}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => <ToBuyList list={item} />}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: colors.black,
+    paddingHorizontal: 30,
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 34,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  add: {
+    color: colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8,
+  },
+});
---
+++ tempData.js
@@ -0,0 +1,94 @@
+export default tempData = [
+    {
+        name: "Padaria",
+        color: "#32CD32",
+        items: [
+            {
+                title: "Pão",
+                completed: true
+            },
+            {
+                title: "Biscoito",
+                completed: false
+            },
+            {
+                title: "Bolo de laranja",
+                completed: false
+            },
+            {
+                title: "Bolo de chocolate",
+                completed: false
+            },
+            {
+                title: "Leite",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Farmácia",
+        color: "#24A6D9",
+        items: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        name: "Papelaria",
+        color: "#8022D9",
+        items: [
+            {
+                title: "Buy Milk",
+                completed: false
+            },
+            {
+                title: "Plan weekend camping trip",
+                completed: true
+            },
+            {
+                title: "Run 2 miles",
+                completed: true
+            },
+            {
+                title: "Make a Video",
+                completed: true
+            },
+            {
+                title: "Take dog for a walk",
+                completed: true
+            }
+        ]
+    },
+    {
+        name: "Carro",
+        color: "#595BD9",
+        items: [
+            {
+                title: "Get Balloons",
+                completed: false
+            },
+            {
+                title: "Send Invitations",
+                completed: true
+            },
+            {
+                title: "Make Dinner Reservations",
+                completed: true
+            }
+        ]
+    }
+];
